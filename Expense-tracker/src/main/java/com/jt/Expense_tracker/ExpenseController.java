8a6fadcb95ff74f39e0b2e75213847db255e60c5
abhisekkundu-create <abhisekkunduel@gmail.com>
2026-09-02 21:16:45 +0530

    Testing postMapping,Dynamic Routing

--- Expense-tracker/src/main/java/com/jt/Expense_tracker/ExpenseController.java
+++ Expense-tracker/src/main/java/com/jt/Expense_tracker/ExpenseController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RestController
 // @RequiredArgsConstructor
 @AllArgsConstructor
+
 public class ExpenseController {
     private final JdbcTemplate jdbcTemplate;
+    private static final String EXPENSES_TABEL="expenses";
 
     // public ExpenseController(JdbcTemplate jdbcTemplate){
     //     this.jdbcTemplate=jdbcTemplate;
@@ -26,7 +30,7 @@ public class ExpenseController {
     @RequestMapping(value="/expenses", method= RequestMethod.GET)
 
     public List<Expense> getExpenses(){
-         String sql="select * from expenses";
+         String sql="select * from %s".formatted(EXPENSES_TABEL);
         //  List<Expense> expenses=new ArrayList<>();
         //  jdbcTemplate.query(query, (resultSet) -> {
         // //  System.out.println("id is" + resultSet.getInt("id"));
@@ -57,6 +61,30 @@ public class ExpenseController {
        
       
 
+    }
+
+    //Dynamic Routing
+
+    @RequestMapping(value = "/expenses/{id}", method =RequestMethod.GET)
+    public Expense getExpenseById(@PathVariable int id){
+
+     System.out.println("id is"+id);
+     var sql="select * from %s where id=?".formatted(EXPENSES_TABEL);
+    Expense expense= jdbcTemplate.queryForObject(sql,new BeanPropertyRowMapper<>(Expense.class),id);
+    return expense;
+
+    }
+
+
+    //@RequestBody-convert json to java object because @RestController conntain 2 annotation like @Controller,@ResponseBody
+
+    @RequestMapping(value = "/expenses",method =RequestMethod.POST )
+    public Expense createExpense(@RequestBody Expense expense){
+        var sql="insert into %s (title,category,price,date) values(?,?,?,?)"
+        .formatted(EXPENSES_TABEL);
+        jdbcTemplate.update(sql,expense.getTitle(),expense.getCategory(),expense.getPrice(),
+        expense.getDate() );
+        return expense;
     }
     
 }
