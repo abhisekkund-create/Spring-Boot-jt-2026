6bcf77e397e2fbbd892bd9d7e2c57d31b312c054
abhisekkundu-create <abhisekkunduel@gmail.com>
2026-09-09 16:36:43 +0530

    Expense-Tracker-Crud by Jpa

---
+++ Expense-tracker - Jpa/src/main/java/com/jt/Expense_tracker/Expense.java
@@ -0,0 +1,34 @@
+package com.jt.Expense_tracker;
+
+import java.time.LocalDate;
+
+// import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity 
+public class Expense {
+    @Id 
+    @GeneratedValue (strategy =GenerationType.IDENTITY )
+    private int id;
+    private String title;
+    private String category;
+    private double price;
+    private LocalDate date;
+
+    // private String description;
+
+    
+
+
+}
---
+++ Expense-tracker - Jpa/src/main/java/com/jt/Expense_tracker/ExpenseController.java
@@ -0,0 +1,104 @@
+package com.jt.Expense_tracker;
+
+// import java.util.ArrayList;
+import java.util.List;
+// import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
+// import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+// import lombok.RequiredArgsConstructor;
+
+@RestController
+
+@AllArgsConstructor
+
+public class ExpenseController {
+   
+    private final ExpenseService expenseService;
+    
+
+    
+
+
+    @RequestMapping(value="/expenses", method= RequestMethod.GET)
+
+    public List<Expense> getExpenses(){
+        //  String sql="select * from %s".formatted(EXPENSES_TABEL);
+       
+        //   return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Expense>(Expense.class));
+       
+    //  var savedExpenses= expenseRepository.findAll();
+    //  return savedExpenses;
+    return expenseService.getExpenses();
+
+    }
+
+   
+
+    @RequestMapping(value = "/expenses/{id}", method =RequestMethod.GET)
+    public Expense getExpenseById(@PathVariable int id){
+
+   //findById(id) return optional class object
+//    1st way
+    // Optional<Expense> optExp =expenseRepository.findById(id);
+    // Expense expense=optExp.orElseThrow(()->new RuntimeException
+    // ("unable to find Expenses with id"+id));
+    // return expense;
+
+    //2nd way
+  
+    return expenseService.getExpenseById(id);
+
+    }
+
+
+
+   
+       @PostMapping("/expenses")
+       @ResponseStatus (code = HttpStatus.CREATED)
+    public Expense createExpense(@RequestBody Expense expense){
+    //   return  expenseRepository.save(expense);
+    return expenseService.addExpense(expense);
+    }
+
+
+
+@DeleteMapping("/expenses/{id}")
+@ResponseStatus (value = HttpStatus.NO_CONTENT)//it create a status code
+    public void deleteExpenses(@PathVariable int id){
+
+//1st way to delete
+    //    Expense tobeDeletedExpense= getExpenseById(id);
+    //    expenseRepository.delete(tobeDeletedExpense);
+
+
+    //2nd way
+    //    expenseRepository.deleteById(id);
+    expenseService.deleteExpenseById(id);
+    }
+
+    
+    //partially update 
+    @PutMapping("/expenses")
+    @ResponseStatus (HttpStatus.ACCEPTED)
+    public Expense updateExpense(@RequestBody Expense expense){
+        
+    return expenseService.updateExpense(expense);
+    
+
+
+    }
+    
+}
